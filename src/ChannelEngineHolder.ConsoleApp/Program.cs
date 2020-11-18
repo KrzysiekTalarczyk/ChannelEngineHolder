5a4f2aee4f5caab1acfd3f44fdf1a3529ab1932c
Krzysztof Talarczyk <kris4dev@gmail.com>
2020-11-18 23:09:59 +0100

    Initial solution

---
+++ src/ChannelEngineHolder.ConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ChannelEngineHolder.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
